9b17bc590717fd073401576ee6ae8ac8bdd54163
LaserToothLiger <lasertoothliger@gmail.com>
2025-12-14 17:17:06 -0800

    v1.1.10
    Code simplification owed to new Framework tool. Tornado Link now tells you its max link range instead of presuming you have read the manual. Compat for Alpha Armoury.

--- 1.6/Source/HautsPsycasts/Class1.cs
+++ 1.6/Source/HautsPsycasts/Class1.cs
@@ -37,11 +37,6 @@ namespace HautsPsycasts
                           postfix: new HarmonyMethod(patchType, nameof(HVP_ThingSetMaker_GeneratePostfix)));
             harmony.Patch(AccessTools.Method(typeof(Hediff_Psylink), nameof(Hediff_Psylink.TryGiveAbilityOfLevel)),
                           postfix: new HarmonyMethod(patchType, nameof(HVP_TryGiveAbilityOfLevelPostfix)));
-            if (ModsConfig.IsActive("metalocif.coolerpsycasts"))
-            {
-                harmony.Patch(AccessTools.Method(typeof(Pawn), nameof(Pawn.Kill)),
-                               prefix: new HarmonyMethod(patchType, nameof(HVP_KillPrefix)));
-            }
             Log.Message("HVP_Initialize".Translate().CapitalizeFirst());
         }
         internal static object GetInstanceField(Type type, object instance, string fieldName)
@@ -133,16 +128,6 @@ namespace HautsPsycasts
                     }
                 }
             }
-        }
-        public static bool HVP_KillPrefix(Pawn __instance)
-        {
-            if (__instance.story != null && __instance.story.traits.HasTrait(HVPDefOf.HVP_Squaddie))
-            {
-                HVPUtility.DisappearPawn(__instance);
-                HealthUtility.HealNonPermanentInjuriesAndRestoreLegs(__instance);
-                return false;
-            }
-            return true;
         }
     }
 
@@ -3252,23 +3237,6 @@ namespace HautsPsycasts
             this.exactRotation += this.rotationRate * deltaTime;
         }
     }
-    //oh-ho, what's this?
-    public class Hediff_ISLF : HediffWithComps
-    {
-        public override void Tick()
-        {
-            base.Tick();
-            if (this.pawn.Downed)
-            {
-                HVPUtility.DisappearPawn(this.pawn);
-            }
-        }
-        public override void PostRemoved()
-        {
-            base.PostRemoved();
-            HVPUtility.DisappearPawn(this.pawn);
-        }
-    }
     //flashstorm buff
     public class CompProperties_AbilityBetterFlashstorm : CompProperties_AbilityEffect
     {
@@ -3696,25 +3664,6 @@ namespace HautsPsycasts
                 }
             }
             return curableHediffs;
-        }
-        public static void DisappearPawn(Pawn pawn)
-        {
-            if (pawn.Spawned)
-            {
-                Map map = pawn.Map;
-                FleckCreationData dataStatic = FleckMaker.GetDataStatic(pawn.Position.ToVector3Shifted(), map, FleckDefOf.PsycastSkipInnerExit, 1f);
-                dataStatic.rotationRate = (float)Rand.Range(-30, 30);
-                dataStatic.rotation = (float)(90 * Rand.RangeInclusive(0, 3));
-                map.flecks.CreateFleck(dataStatic);
-                FleckCreationData dataStatic2 = FleckMaker.GetDataStatic(pawn.Position.ToVector3Shifted(), map, FleckDefOf.PsycastSkipOuterRingExit, 1f);
-                dataStatic2.rotationRate = (float)Rand.Range(-30, 30);
-                dataStatic2.rotation = (float)(90 * Rand.RangeInclusive(0, 3));
-                map.flecks.CreateFleck(dataStatic2);
-                SoundDefOf.Psycast_Skip_Exit.PlayOneShot(new TargetInfo(pawn.Position, map, false));
-                pawn.teleporting = true;
-                pawn.ExitMap(false, Rot4.Invalid);
-                pawn.Destroy();
-            }
         }
     }
     public class HVP_Settings : ModSettings
